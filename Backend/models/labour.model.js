b6180f98448f83c3387fc2826e09ac524f357d37
Mohammad Sahil <mohammadsahil4343@gmail.com>
2024-05-08 09:57:37 +0530

    Backend

---
+++ Backend/models/labour.model.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+const labourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    mobile_number: {
+        type: Number,
+        required: true,
+        unique : true
+    },
+
+    password : {
+        type: String,
+        required : true
+    },
+    address: {
+        address1 : {
+            type : String,
+            required : true
+        },
+        district: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        pin_code: {
+            type: Number,
+            required: true
+        }
+    },
+    trade: {
+        type: String,
+        required: true
+    },
+    photo: {
+        data: Buffer,
+        contentType: String 
+    },
+    aadhaar_no: {
+        type: Number
+    },
+    account_details: {
+        account_num : {
+            type : Number,
+        } ,
+        ifsc : {
+            type: String
+        }
+    },
+    reviews: {
+        upvote: {
+            type: [String],
+            default: []
+        },
+        feedback: {
+            type: [String],
+            default: []
+        }
+    }
+} , {timestamps : true , versionKey : false});
+
+module.exports = mongoose.model("Labour", labourSchema);
---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,10 @@
+const labourController = require("../controllers/labour.controller")
+const clientController = require("../controllers/client.controller")
+
+module.exports = (app) =>{
+    app.post("/karmMitra/labour/signup" , labourController.signup)
+    app.post("/karmMitra/labour/signin" , labourController.signin)
+    app.post("/karmMitra/client/signup" , clientController.signup)
+    app.post("/karmMitra/client/signin" , clientController.signin)
+
+}
